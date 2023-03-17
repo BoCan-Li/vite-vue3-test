330f718ebaa41b552a3dd2e3aa6efa7787a84bee
NewLiMan <17366285416@163.com>
2023-03-17 16:44:13 +0800

    调试demo

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-vue3-test/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
